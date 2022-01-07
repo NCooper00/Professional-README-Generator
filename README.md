638fcbf3c4d5175cff4db9d43d940ee8e4d2dfb5
Nick Cooper <nccooper00@gmail.com>
2022-01-07 02:52:13 -0800

    Added the array and a simple function to input user comments into the README.md file

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Professional-README-Generator
-Creating a readme generator using the inquirer package to dynamically add details to the readme based on the user's input.
+# kajhdaksjh
+
+## Description
+        
+adiuashdkjashda
+        
+## Table of Contents
+        
+Listed below are shortcuts to different parts of the README.
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Tests](#tests)
+- [License](#liscense)
+- [Features](#features)
+- [Questions](#questions)
+        
+## Installation
+        
+kjadhaksjdhkasd
+        
+## Usage
+        
+kjahdkasjhdaks
+        
+## Credits
+        
+kjhadkjahsdkj
+        
+## Tests 
+        
+kjhjkahdkajsd
+        
+## Features
+        
+jhakjdhaksjdha
+
+## Questions
+
+GitHub Username: kjhadkjahdkjas
+https://github.com/kjhadkjahdkjas
+
+Email Address: lhadjklahsdka
--- index.js
+++ index.js
@@ -1,13 +1,155 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = {
+    title: 'What is the title of your project?',
+    description: 'Write a description of your project and what it does.',
+    installation: 'Give instructions on how to install or use the application you created.',
+    usage: 'Provide instructions and examples of use for your project.',
+    credits: 'List any collaborators or links to any resources you used.',
+    tests: 'Write tests for your project, and examples of how to run them.',
+    features: 'List the features of your project!',
+    github: 'What is your GitHub username?',
+    email: 'What is your email address?'
+};
+
+const {title, description, installation, usage, credits, tests, features, github, email} = questions;
+
+inquirer    
+    .prompt([
+        {
+            name: 'title',
+            type: 'input',
+             message: title,
+        },
+        {
+            name: 'description',
+            type: 'input',
+            message: description,
+        },
+        {
+            name: 'installation',
+            type: 'input',
+            message: installation,
+        },
+        {
+            name: 'usage',
+            type: 'input',
+            message: usage,
+        },
+        {
+            name: 'credits',
+            type: 'input',
+            message: credits,
+        },
+        {
+            name: 'tests',
+            type: 'input',
+            message: tests,
+        },
+        {
+            name: 'features',
+            type: 'input',
+            message: features,
+        },
+        {
+            name: 'github',
+            type: 'input',
+            message: github,
+        },
+        {
+            name: 'email',
+            type: 'input',
+            message: email,
+        },
+    ])
+    .then((response) => {
+        console.log(response)
+
+    fs.writeFile(
+
+'README.md',
+
+`# ${response.title}
+
+## Description
+        
+${response.description}
+        
+## Table of Contents
+        
+Listed below are shortcuts to different parts of the README.
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Tests](#tests)
+- [License](#liscense)
+- [Features](#features)
+- [Questions](#questions)
+        
+## Installation
+        
+${response.installation}
+        
+## Usage
+        
+${response.usage}
+        
+## Credits
+        
+${response.credits}
+        
+## Tests 
+        
+${response.tests}
+        
+## Features
+        
+${response.features}
+
+## Questions
+
+GitHub Username: ${response.github}
+https://github.com/${response.github}
+
+You can contact me with this email address!
+Email Address: ${response.email}
+`,
+
+        (err) => err ? console.error(err) : console.log('README generated!'));
+    });
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(
+
+        'fileName',
+
+        `data`,
+
+        (err) => err ? console.error(err) : console.log('README generated!'));
+    
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    const questions = {
+        title: 'What is the title of your project?',
+        description: 'Write a description of your project and what it does.',
+        installation: 'Give instructions on how to install or use the application you created.',
+        usage: 'Provide instructions and examples of use for your project.',
+        credits: 'List any collaborators or links to any resources you used.',
+        tests: 'Write tests for your project, and examples of how to run them.',
+        features: 'List the features of your project!'
+    };
+
+    const {title, description, installation, usage, credits, tests, features} = questions;
+}
 
 // Function call to initialize app
 init();
